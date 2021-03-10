6bd4e3bce3282a6f41956b9334421286f0f0fb4b
Vitalii Skyrtach <vitalijskirtac@gmail.com>
2021-03-10 10:58:37 +0200

    first commit

--- js/dz-31-2.js
+++ js/dz-31-2.js
@@ -7,29 +7,32 @@ let balance = {
 let dzBalance = document.getElementById("dzBalance");
 let showBalance = document.getElementById("showBalanceDZ31");
 
-if(dzBalance){
+if (dzBalance) {
     showBalance.addEventListener("click", showBalanceDZ31);
 }
 
-function showBalanceDZ31(){
+function showBalanceDZ31() {
     localStorage.setItem("dzBalance", JSON.stringify(balance));
     console.log("-------");
     let totalBalance = balance.full - balance.reserved;
-    dzBalance.innerText = "Баланс - " + totalBalance + " " + balance.currency +" (reserved " + balance.reserved + " "  + balance.currency + ")";
+    dzBalance.innerText = "Баланс - " + totalBalance + " " + balance.currency + " (reserved " + balance.reserved + " " + balance.currency + ")";
 }
 
 let fullBalance = document.getElementById("fullBalance");
 let changeBalance = document.getElementById("changeBalanceDZ31");
 
-changeBalance.addEventListener("click", changeBalanceDZ31);
 
-function changeBalanceDZ31(){
+if (changeBalance) {
+    changeBalance.addEventListener("click", changeBalanceDZ31);
+}
+
+function changeBalanceDZ31() {
     let inputBalance = Number(document.getElementById("dzBalance__input").value);
-    if(inputBalance < 0){
+    if (inputBalance < 0) {
         alert("Значення яке ви намагаєтеся встановити менше ніж 0");
-    }else if(inputBalance < balance.reserved){
+    } else if (inputBalance < balance.reserved) {
         alert("Значення яке ви намагаєтеся встановити менше reserved");
-    }else{
+    } else {
         balance.full = inputBalance;
         fullBalance.innerText = inputBalance;
         console.log(inputBalance);
